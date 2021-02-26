aa6e4fb238ed3fd8198784f1142c8da44c5803bc
s2oBCN <sergio.sacristan@boehringer-ingelheim.com>
2021-02-26 11:03:43 +0100

    GenerateJiraJson

--- test/groovy/org/ods/orchestration/usecase/VersionHistorySpec.groovy
+++ test/groovy/org/ods/orchestration/usecase/VersionHistorySpec.groovy
@@ -1,8 +1,9 @@
 package org.ods.orchestration.usecase
 
 import com.github.tomakehurst.wiremock.core.Options
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.github.tomakehurst.wiremock.junit.WireMockRule
-import groovy.util.logging.Log
+import groovy.util.logging.Slf4j
 import org.apache.commons.io.FileUtils
 import org.junit.Rule
 import org.junit.contrib.java.lang.system.EnvironmentVariables
@@ -25,8 +26,9 @@ import spock.lang.Specification
 import util.FixtureHelper
 import util.LoggerStub
 import util.PipelineSteps
+import static com.github.tomakehurst.wiremock.client.WireMock.*
 
-@Log
+@Slf4j
 class VersionHistorySpec  extends Specification {
 
     @Rule
@@ -36,7 +38,8 @@ class VersionHistorySpec  extends Specification {
     public TemporaryFolder tempFolder
 
     @Rule
-    public WireMockRule jiraWireMockRule = new WireMockRule(Options.DYNAMIC_PORT);
+    public WireMockRule jiraServer = new WireMockRule(
+        (WireMockConfiguration.wireMockConfig().withRootDirectory("test/resources/wiremock").dynamicPort()))
 
     Project project
     IPipelineSteps steps
@@ -56,6 +59,64 @@ class VersionHistorySpec  extends Specification {
 
     def setup() {
         log.info "Using temporal folder:${tempFolder.getRoot()}"
+
+        jenkins = Mock(JenkinsService)
+        nexus = Mock(NexusService)
+        os = Mock(OpenShiftService)
+        sq = Mock(SonarQubeUseCase)
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10000"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10000.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10001"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10001.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10102"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10102.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10200"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10200.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10201"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10201.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10202"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10202.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10203"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10203.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10204"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10204.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10205"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10205.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/createmeta/NET/issuetypes/10206"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/issueTypes-10206.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/api/2/issue/NET-123?fields=customfield_10222"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/customField_10222.json")));
+        jiraServer.stubFor(
+            get(urlEqualTo("/rest/platform/1.0/docgenreports/NET"))
+                .willReturn(aResponse().withStatus(200)
+                    .withBodyFile("jira/docgenreports.json")));
+
         System.setProperty("java.io.tmpdir", tempFolder.getRoot().absolutePath)
         FileUtils.copyDirectory(new FixtureHelper().getResource("Test-1.pdf").parentFile, tempFolder.getRoot());
 
@@ -63,19 +124,15 @@ class VersionHistorySpec  extends Specification {
         pdfUtil = new PDFUtil()
         logger = new LoggerStub(true)
 
-        jenkins = Mock(JenkinsService)
         jenkins.unstashFilesIntoPath(_, _, "SonarQube Report") >> true
-        project = Spy(buildProject())
-        util = Spy(new MROPipelineUtil(project, steps, null, logger))
-        docGen = Mock(DocGenService)
-        junit = Spy(new JUnitTestReportsUseCase(project, steps))
-        levaFiles = Mock(LeVADocumentChaptersFileService)
-        nexus = Mock(NexusService)
-        os = Mock(OpenShiftService)
-        sq = Mock(SonarQubeUseCase)
-        def jiraService = new JiraService("http://localhost:${jiraWireMockRule.port()}", "username", "password")
+        project = buildProject() //Spy(buildProject())
+        util = new MROPipelineUtil(project, steps, null, logger) // TODO Spy(new MROPipelineUtil(project, steps, null, logger))
+        docGen = new DocGenService("http://localhost") //TODO Mock(DocGenService)
+        junit = new JUnitTestReportsUseCase(project, steps) // TODO Spy(new JUnitTestReportsUseCase(project, steps))
+        levaFiles = new LeVADocumentChaptersFileService(steps) // TODO Mock(LeVADocumentChaptersFileService)
+        def jiraService = new JiraService("http://localhost:${jiraServer.port()}", "username", "password")
         jiraUseCase = new JiraUseCase(project, steps, util, jiraService, logger)
-        usecase = Spy(new LeVADocumentUseCase(project, steps, util, docGen, jenkins, jiraUseCase, junit, levaFiles, nexus, os, pdfUtil, sq))
+        usecase = new LeVADocumentUseCase(project, steps, util, docGen, jenkins, jiraUseCase, junit, levaFiles, nexus, os, pdfUtil, sq)
         project.load(Mock(GitService), jiraUseCase)
     }
 
@@ -88,6 +145,7 @@ class VersionHistorySpec  extends Specification {
         project.data.buildParams.targetEnvironment = "dev"
         project.data.buildParams.targetEnvironmentToken = "D"
         project.data.buildParams.version = "WIP"
+        project.data.buildParams.releaseStatusJiraIssueKey = "NET-123"
         project.getOpenShiftApiUrl() >> 'https://api.dev-openshift.com'
         return project
     }
--- test/groovy/util/GenerateJiraJson.groovy
+++ test/groovy/util/GenerateJiraJson.groovy
@@ -1,4 +1,45 @@
 package util
 
+import groovy.json.JsonSlurperClassic
+import kong.unirest.Unirest
+import org.ods.orchestration.service.JiraService
+
 class GenerateJiraJson {
+
+    public static final String JIRA_JSON_FILES = "build/tmp/json/jira"
+
+    static void main(String[] args) {
+        def projectKey = ""
+        def version = ""
+        new GenerateJiraJson().start(projectKey, version)
+    }
+
+    void start(projectKey, version) {
+        jiraOutputDirExist()
+        def service = new JiraJsonService("", "openshift", "openshift")
+
+        generateJsonFile("getDocGenData", service.getDocGenData(projectKey))
+        generateJsonFile("getDeltaDocGenData", service.getDeltaDocGenData(projectKey, version))
+       // generateJsonFile("getDocGenData", service.getIssueTypeMetadata(projectKey, issueTypeId))
+        generateJsonFile("getIssueTypes", service.getIssueTypes(projectKey))
+        generateJsonFile("getVersionsForProject", service.getVersionsForProject(projectKey))
+        generateJsonFile("getProject", service.getProject(projectKey))
+        generateJsonFile("getProjectVersions", service.getProjectVersions(projectKey))
+        //generateJsonFile("getLabelsFromIssue", service.getLabelsFromIssue(issueIdOrKey))
+        //generateJsonFile("searchByJQLQuery", service.searchByJQLQuery(query))
+       // generateJsonFile("getTextFieldsOfIssue", service.getTextFieldsOfIssue(issueIdOrKey, List fields))
+        //generateJsonFile("getTextFieldsOfIssue", service.isVersionEnabledForDelta(projectKey, versionName))
+
+    }
+
+    private void generateJsonFile(String fileName, def jsonData) {
+        new File("${JIRA_JSON_FILES}/${fileName}").text = jsonData
+    }
+
+    private void jiraOutputDirExist() {
+        def json_path = new File(JIRA_JSON_FILES)
+        if (json_path.exists()) {
+            json_path.deleteDir()
+        }
+    }
 }
