c1c5c09fa6873147cb8665178bc16a25491641fc
zxBCN Rodriguez_Cornejo,Hector (IT EDS) EXTERNAL <hector.rodriguez_cornejo.ext@boehringer-ingelheim.com>
2021-03-17 13:41:44 +0100

    Integrated in SSDS doc gen

--- src/org/ods/orchestration/InitStage.groovy
+++ src/org/ods/orchestration/InitStage.groovy
@@ -194,6 +194,23 @@ class InitStage extends Stage {
             )
         )
 
+        def bitbucket = BitbucketService.newFromEnv(
+            steps.unwrap(),
+            steps.env,
+            project.key,
+            project.services.bitbucket.credentials.id,
+            logger
+        )
+        registry.add(BitbucketService, bitbucket)
+
+        registry.add(BitbucketTraceabilityUseCase,
+            new BitbucketTraceabilityUseCase(
+                registry.get(BitbucketService),
+                registry.get(PipelineSteps),
+                registry.get(Project)
+            )
+        )
+
         registry.add(LeVADocumentUseCase,
             new LeVADocumentUseCase(
                 registry.get(Project),
@@ -207,7 +224,8 @@ class InitStage extends Stage {
                 registry.get(NexusService),
                 registry.get(OpenShiftService),
                 registry.get(PDFUtil),
-                registry.get(SonarQubeUseCase)
+                registry.get(SonarQubeUseCase),
+                registry.get(BitbucketTraceabilityUseCase)
             )
         )
 
@@ -220,15 +238,6 @@ class InitStage extends Stage {
                 logger
             )
         )
-
-        def bitbucket = BitbucketService.newFromEnv(
-            steps.unwrap(),
-            steps.env,
-            project.key,
-            project.services.bitbucket.credentials.id,
-            logger
-        )
-        registry.add(BitbucketService, bitbucket)
 
         git.configureUser()
         steps.withCredentials(
